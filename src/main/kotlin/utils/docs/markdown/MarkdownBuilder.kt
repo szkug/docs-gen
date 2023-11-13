d23ca82e958dd38193574450545dbd10ae99bf8d
korilin <korilin.dev@gmail.com>
2023-11-13 10:24:44 +0800

    fix: remove jump title point

--- src/main/kotlin/utils/docs/markdown/MarkdownBuilder.kt
+++ src/main/kotlin/utils/docs/markdown/MarkdownBuilder.kt
@@ -53,7 +53,7 @@ fun DIV.buildContent(file: File) = with(MarkdownBuildConfig) {
     val content = file.readText()
     val tree = parser.buildMarkdownTreeFromString(content)
     val generator = HtmlGenerator(content, tree, flavour)
-    val html = generator.generateHtml(visitor)
+    val html = generator.generateHtml()
 
     div(classes = "markdown-content") {
 
